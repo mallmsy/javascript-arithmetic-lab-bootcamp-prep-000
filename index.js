e3abdf74e54ddaf589371a6cc3cae648d760b3d6
Mallory Woods <malvwoods@gmail.com>
2019-02-11 01:40:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ b = Math.floor(Math.random() * 1000)
 
 function add() {
   a = Math.floor(Math.random() * 1000)
-b = Math.floor(Math.random() * 1000)
+  b = Math.floor(Math.random() * 1000)
  
   return(`${a} + ${b}`)}
 
